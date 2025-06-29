c331a80ea125f9d5519a61dfe6eefcf8e4fbf3b0
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-06-29 16:58:17 -0300

    Módulo Avançado · Capítulo 15 · Avaliação de Conhecimentos

---
+++ capitulo-15/knowledge_assessment.py
@@ -0,0 +1,22 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: knowledge_assessment.py
+# DATA DE CRIAÇÃO: 29-06-2025
+# DATA DE PUBLICAÇÃO: 29-06-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Avançado
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Questão 01-) Quando comparamos os mecanismos de list comprehension e de expressões geradoras:
+# I. O list comprehension gera todos os elementos da sequência estabelecida, requerendo maior quantidade de memória para seu armazenamento.
+# II. A expressão geradora gera cada elemento da sequência estabelecida apenas no momento que é necessário.
+# III. O processamento de uma expressão geradora é usualmente mais lento do que uma list comprehension equivalente.
+# Resposta: d. todas as afirmativas estão corretas.
+
+# Questão 02-) Sobre as funções geradoras do Python:
+# I. Podem ser criadas tal como funções comuns, sem qualquer exigência ou restrição especial.
+# II. É um tipo especial de função que implementa a estratégia conhecida por avaliação preguiçosa.
+# III. São funções que retornam um objeto que pode ser usado como iterável em um laço for.
+# Resposta: c. apenas as afirmativas II e III estão corretas.
