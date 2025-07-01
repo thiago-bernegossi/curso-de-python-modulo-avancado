558c1ab5ee53b0ace36493d11b8510a63bd65c03
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-07-01 19:59:27 -0300

    Módulo Avançado · Atualizando o arquivo module_namespace.py

--- capitulo-16/module_namespace.py
+++ capitulo-16/module_namespace.py
@@ -1,7 +1,7 @@
 # ---------------------------------------------------------------------------------------------------------------------
 # ARQUIVO: module_namespace.py
 # DATA DE CRIAÇÃO: 30-06-2025
-# DATA DE PUBLICAÇÃO: 30-06-2025
+# DATA DE PUBLICAÇÃO: 01-07-2025
 # CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
 # CONTEÚDO OFERTADO: Curso de Python – Módulo Avançado
 # DOCENTE AUTOR: Sérgio Luiz Banin
@@ -13,7 +13,7 @@ import utilities_module
 
 print('A execução do sistema informático foi iniciada.\n')
 
-print('Exibindo o namespace global do módulo específico gerado pelo interpretador Python após a inicialização:')
+print('Exibindo todos os elementos disponíveis no namespace do módulo específico gerado pelo interpretador Python após a inicialização:')
 print(dir(utilities_module))
 
 print(f'\nA execução do sistema informático foi concluída.')
