45d746a6efbca4dfeedcbdaebf03d357d445fed2
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-07-02 19:23:04 -0300

    Módulo Avançado · Capítulo 16 · Avaliação de Conhecimentos

---
+++ capitulo-16/knowledge_assessment.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: knowledge_assessment.py
+# DATA DE CRIAÇÃO: 02-07-2025
+# DATA DE PUBLICAÇÃO: 02-07-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Avançado
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Questão 01-) O mecanismo de modularização oferecido pela linguagem Python oferece uma série de vantagens. Qual alternativa indica corretamente tais vantagens?
+# Resposta: d. Simplicidade, capacidade de manutenção, reutilização, melhorias no escopo.
+
+# Questão 02-) Programas, de diferentes complexidades, criam muitos identificadores ou nomes para seu objetos. Para gerenciar essa questão, o Python incorpora o conceito de namespace, uma coleção de diferentes objetos criados em memória sendo que a cada um está associado a um nome único em determinado momento.
+# I. O Python oferece quatro tipos de namespace: built-in, global, local e non-local.
+# II. O namespace built-in contém os nomes dos objetos embutidos do Python, como int, float, list, print e outros.
+# III. O namespace global contém os nomes definidos no âmbito do programa principal.
+# Resposta: d. todas as afirmativas estão corretas.
