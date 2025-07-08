1af1ec98fe9bafdea473bd1ce213ad3cfd32bc12
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-07-07 23:33:59 -0300

    Módulo Avançado · Capítulo 18 · Avaliação de Conhecimentos

---
+++ capitulo-18/knowledge_assessment.py
@@ -0,0 +1,23 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: knowledge_assessment.py
+# DATA DE CRIAÇÃO: 07-07-2025
+# DATA DE PUBLICAÇÃO: 07-07-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Avançado
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Questão 01-) Os sistemas gerenciadores de banco de dados (SGBD) são softwares especializados no controle do armazenamento, da recuperação e da manutenção de grandes volumes de dados. Dentre as várias tecnologias de bancos de dados, um grupo muito predominante é o dos sistemas gerenciadores de banco de dados relacionais (SGBDR).
+# Sobre os bancos de dados relacionais:
+# I. Nos bancos de dados relacionais, os dados são organizados em tabelas, que contém registros (linhas), que são um conjunto de dados relacionados, os quais são distribuídos nos campos (colunas) das tabelas.
+# II. A Structured Query Language (SQL) é uma linguagem de propósito geral, que serve tanto para interagir com bancos de dados relacionais, como para escrever programas em geral.
+# III. Existem muitos produtos consagrados no mercado de bancos de dados relacionais, como Oracle DB, Microsoft SQLServer, MySQL e outros.
+# Resposta: b. apenas as afirmativas I e III estão corretas.
+
+# Questão 02-) Considerando o uso do SQLite, como banco de dados (BD) de um programa Python:
+# I. A função sqlite3.connect() permite tanto a conexão com um BD existente, como a criação do BD caso não exista.
+# II. A partir da conexão com o BD é necessária a criação de um cursor para o envio de comandos.
+# III. A execução de comandos que modificam o BD, como operações de inclusão, alteração ou remoção de registros, requerem a execução do método commit(). para salvar as modificações efetuadas.
+# Resposta: d. todas as afirmativas estão corretas.
